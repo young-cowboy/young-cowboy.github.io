0f04304aa85bcb1a02c16dc06a55d0aaa8cb62ef
yingyi.zj <yingyi.zj@alibaba-inc.com>
2017-06-21 19:18:42 +0800

    change path

---
+++ gallery/mediadevices/polyfill.js
@@ -0,0 +1,18 @@
+// getUserMedia 被标准废除了，mediaDevices 正在取代中
+if (navigator.mediaDevices === undefined) {
+    navigator.mediaDevices = {};
+}
+
+if (navigator.mediaDevices.getUserMedia === undefined) {
+    navigator.mediaDevices.getUserMedia = function(constraints) {
+        var getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+        if (!getUserMedia) {
+            return Promise.reject(new Error('getUserMedia is not implemented in this browser'));
+        }
+        return new Promise(function(resolve, reject) {
+            getUserMedia.call(navigator, constraints, resolve, reject);
+        });
+    }
+}
+
+window.URL = (window.URL || window.webkitURL || window.mozURL || window.msURL);
